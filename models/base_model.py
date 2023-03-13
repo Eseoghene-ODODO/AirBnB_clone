87fe978dc0e867a11a1231c0d138a8e640adf944
Mobola Falomo <mobolafalomo@gmail.com>
2023-03-13 15:04:55 -0700

    Solutions to task 0-17

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """ Module containing BaseModel """
 import uuid
 from datetime import datetime
-#from models import storage
 
 
 class BaseModel:
@@ -20,23 +19,24 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.strptime(value,
-                                                         '%Y-%m-%dT%H:%M:%S.%f'))
+                            '%Y-%m-%dT%H:%M:%S.%f'))
                 elif key != '__class__':
                     setattr(self, key, value)
 
     def __str__(self):
         """ Returns a string representation of the BaseModel instance """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self.__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
         """ Saves the BaseModel instance """
-        self.updated_at = datetime.now()
+        self.updated_at=datetime.now()
         storage.save()
 
     def to_dict(self):
         """ Returns a dictionary representation of the BaseModel instance """
-        dictionary = self.__dict__.copy()
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary['__class__'] = type(self).__name__
+        dictionary=self.__dict__.copy()
+        dictionary['created_at']=self.created_at.isoformat()
+        dictionary['updated_at']=self.updated_at.isoformat()
+        dictionary['__class__']=type(self).__name__
         return dictionary
